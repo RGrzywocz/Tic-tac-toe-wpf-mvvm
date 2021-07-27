a90cd6587b20b55d81d2a426a87308c857fa0ceb
Rafał Grzywocz <49996052+RGrzywocz@users.noreply.github.com>
2021-07-27 14:40:30 +0200

    Add project files.

---
+++ Model/Game.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace tic_tac_toe.Model
+{
+    class Game
+    {
+        public string[] board = new string[9] { " ", " ", " ", " ", " ", " ", " ", " ", " " };
+        public bool[] emptyField = new bool[] { true, true, true, true, true, true, true, true, true };
+        public bool isXNext = true;
+        public bool isGameOver = false;
+
+        public string nextPlayer = "X\nturn";
+        public string nextPlayerString()
+        {
+            return isXNext ? "X\nTurn" : "O\nTurn";
+        }
+        public void newGame()
+        {
+            for (int i = 0; i < 9; i++)
+            {
+                emptyField[i] = true;
+                board[i] = " ";
+            }
+            isXNext = true;
+        }
+        public void buttonClick(int button)
+        {
+            if (isXNext)
+            {
+                board[button] = "X";
+            }
+            else
+            {
+                board[button] = "O";
+            }
+            isXNext = !isXNext;
+            emptyField[button] = false;
+        }
+        public bool checkGame()
+        {
+            nextPlayer = nextPlayerString();
+            bool gameOver = true;
+            bool xWon = false;
+            bool yWon = false;
+            if (board[0] == "X" && board[1] == "X" && board[2] == "X")
+            {
+                xWon = true;
+                gameOver = true;
+            }
+            if (board[3] == "X" && board[4] == "X" && board[5] == "X")
+            {
+                xWon = true;
+                gameOver = true;
+            }
+            if (board[6] == "X" && board[7] == "X" && board[8] == "X")
+            {
+                xWon = true;
+                gameOver = true;
+            }
+            if (board[0] == "X" && board[3] == "X" && board[6] == "X")
+            {
+                xWon = true;
+                gameOver = true;
+            }
+            if (board[1] == "X" && board[4] == "X" && board[7] == "X")
+            {
+                xWon = true;
+                gameOver = true;
+            }
+            if (board[2] == "X" && board[5] == "X" && board[8] == "X")
+            {
+                xWon = true;
+                gameOver = true;
+            }
+            if (board[0] == "X" && board[4] == "X" && board[8] == "X")
+            {
+                xWon = true;
+                gameOver = true;
+            }
+            if (board[2] == "X" && board[4] == "X" && board[6] == "X")
+            {
+                xWon = true;
+                gameOver = true;
+            }
+            if (board[0] == "O" && board[1] == "O" && board[2] == "O")
+            {
+                yWon = true;
+                gameOver = true;
+            }
+            if (board[3] == "O" && board[4] == "O" && board[5] == "O")
+            {
+                yWon = true;
+                gameOver = true;
+            }
+            if (board[6] == "O" && board[7] == "O" && board[8] == "O")
+            {
+                yWon = true;
+                gameOver = true;
+            }
+            if (board[0] == "O" && board[3] == "O" && board[6] == "O")
+            {
+                yWon = true;
+                gameOver = true;
+            }
+            if (board[1] == "O" && board[4] == "O" && board[7] == "O")
+            {
+                yWon = true;
+                gameOver = true;
+            }
+            if (board[2] == "O" && board[5] == "O" && board[8] == "O")
+            {
+                yWon = true;
+                gameOver = true;
+            }
+            if (board[0] == "O" && board[4] == "O" && board[8] == "O")
+            {
+                yWon = true;
+                gameOver = true;
+            }
+            if (board[2] == "O" && board[4] == "O" && board[6] == "O")
+            {
+                yWon = true;
+                gameOver = true;
+            }
+            if (!yWon && !xWon)
+            {
+                for (int i = 0; i < 9; i++)
+                {
+                    if (board[i] == " " || board[i] == "")
+                    {
+                        gameOver = false;
+                    }
+                }
+            }
+            if (!gameOver)
+            {
+                return gameOver;
+            }
+            if (xWon)
+            {
+                MessageBox.Show("Player X won !", "Victory !");
+                newGame();
+                return gameOver;
+            }
+            if (yWon)
+            {
+                MessageBox.Show("Player O won !", "Victory !");
+                newGame();
+                return gameOver;
+            }
+            MessageBox.Show("It's a draw", "Game Over !");
+            newGame();
+            return gameOver;
+
+        }
+
+    }
+}
---
+++ ViewModel/MainViewModel.cs
@@ -0,0 +1,279 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace tic_tac_toe.ViewModel
+{
+    class MainViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        private Model.Game game = new Model.Game();
+
+        public string[] Board
+        {
+            get
+            {
+                return game.board;
+            }
+            set
+            {
+                game.board = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Board)));
+            }
+        }
+        public bool[] ButtonsUse
+        {
+            get
+            {
+                return game.emptyField;
+            }
+            set
+            {
+                game.emptyField = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(ButtonsUse)));
+            }
+        }
+
+        public string NextMove
+        {
+            get
+            {
+                return game.nextPlayer;
+            }
+            set
+            {
+                game.nextPlayer = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(NextMove)));
+            }
+        }
+        private ICommand newGame;
+        public ICommand NewGame
+        {
+            get
+            {
+                return newGame ?? (newGame = new BaseClass.RelayCommand((p) =>
+                {
+                    Board = new string[9]{ "", "", "", "", "", "", "", "", ""};
+                    NextMove = "X\nmove";
+                    ButtonsUse = new bool[9] { true, true, true, true, true, true, true, true, true };
+                    game.newGame();
+                }, p => true));
+            }
+        }
+
+        private ICommand buttonClick0, buttonClick1, buttonClick2, buttonClick3, buttonClick4, buttonClick5, buttonClick6, buttonClick7, buttonClick8;
+        public ICommand ButtonClick0
+        {
+            get
+            {
+                return buttonClick0 ?? (buttonClick0 = new BaseClass.RelayCommand((p) =>
+                {
+                    game.buttonClick(0);
+                    NextMove = game.nextPlayerString();
+                    string[] temp = (string[])Board.Clone();
+                    temp[0] = game.board[0];
+                    Board = temp;
+                    bool[] temp2 = (bool[])ButtonsUse.Clone();
+                    temp2[0] = game.emptyField[0];
+                    ButtonsUse = temp2;
+                    if (game.checkGame())
+                    {
+                        ButtonsUse = new bool[9] { true, true, true, true, true, true, true, true, true };
+                        Board = new string[9] { "", "", "", "", "", "", "", "", "" };
+                        NextMove = "X\nmove";
+                    }
+                }, p => true));
+            }
+        }
+        public ICommand ButtonClick1
+        {
+            get
+            {
+                return buttonClick1 ?? (buttonClick1 = new BaseClass.RelayCommand((p) =>
+                {
+                    game.buttonClick(1);
+                    NextMove = game.nextPlayerString();
+                    string[] temp = (string[])Board.Clone();
+                    temp[1] = game.board[1];
+                    Board = temp;
+                    bool[] temp2 = (bool[])ButtonsUse.Clone();
+                    temp2[1] = game.emptyField[1];
+                    ButtonsUse = temp2;
+                    if (game.checkGame())
+                    {
+                        ButtonsUse = new bool[9] { true, true, true, true, true, true, true, true, true };
+                        Board = new string[9] { "", "", "", "", "", "", "", "", "" };
+                        NextMove = "X\nmove";
+                    }
+                }, p => true));
+            }
+        }
+        public ICommand ButtonClick2
+        {
+            get
+            {
+                return buttonClick2 ?? (buttonClick2 = new BaseClass.RelayCommand((p) =>
+                {
+                    game.buttonClick(2);
+                    NextMove = game.nextPlayerString();
+                    string[] temp = (string[])Board.Clone();
+                    temp[2] = game.board[2];
+                    Board = temp;
+                    bool[] temp2 = (bool[])ButtonsUse.Clone();
+                    temp2[2] = game.emptyField[2];
+                    ButtonsUse = temp2;
+                    if (game.checkGame())
+                    {
+                        ButtonsUse = new bool[9] { true, true, true, true, true, true, true, true, true };
+                        Board = new string[9] { "", "", "", "", "", "", "", "", "" };
+                        NextMove = "X\nmove";
+                    }
+                }, p => true));
+            }
+        }
+        public ICommand ButtonClick3
+        {
+            get
+            {
+                return buttonClick3 ?? (buttonClick3 = new BaseClass.RelayCommand((p) =>
+                {
+                    game.buttonClick(3);
+                    NextMove = game.nextPlayerString();
+                    string[] temp = (string[])Board.Clone();
+                    temp[3] = game.board[3];
+                    Board = temp;
+                    bool[] temp2 = (bool[])ButtonsUse.Clone();
+                    temp2[3] = game.emptyField[3];
+                    ButtonsUse = temp2;
+                    if (game.checkGame())
+                    {
+                        ButtonsUse = new bool[9] { true, true, true, true, true, true, true, true, true };
+                        Board = new string[9] { "", "", "", "", "", "", "", "", "" };
+                        NextMove = "X\nmove";
+                    }
+                }, p => true));
+            }
+        }
+        public ICommand ButtonClick4
+        {
+            get
+            {
+                return buttonClick4 ?? (buttonClick4 = new BaseClass.RelayCommand((p) =>
+                {
+                    game.buttonClick(4);
+                    NextMove = game.nextPlayerString();
+                    string[] temp = (string[])Board.Clone();
+                    temp[4] = game.board[4];
+                    Board = temp;
+                    bool[] temp2 = (bool[])ButtonsUse.Clone();
+                    temp2[4] = game.emptyField[4];
+                    ButtonsUse = temp2;
+                    if (game.checkGame())
+                    {
+                        ButtonsUse = new bool[9] { true, true, true, true, true, true, true, true, true };
+                        Board = new string[9] { "", "", "", "", "", "", "", "", "" };
+                        NextMove = "X\nmove";
+                    }
+                }, p => true));
+            }
+        }
+        public ICommand ButtonClick5
+        {
+            get
+            {
+                return buttonClick5 ?? (buttonClick5 = new BaseClass.RelayCommand((p) =>
+                {
+                    game.buttonClick(5);
+                    NextMove = game.nextPlayerString();
+                    string[] temp = (string[])Board.Clone();
+                    temp[5] = game.board[5];
+                    Board = temp;
+                    bool[] temp2 = (bool[])ButtonsUse.Clone();
+                    temp2[5] = game.emptyField[5];
+                    ButtonsUse = temp2;
+                    if (game.checkGame())
+                    {
+                        ButtonsUse = new bool[9] { true, true, true, true, true, true, true, true, true };
+                        Board = new string[9] { "", "", "", "", "", "", "", "", "" };
+                        NextMove = "X\nmove";
+                    }
+                }, p => true));
+            }
+        }
+        public ICommand ButtonClick6
+        {
+            get
+            {
+                return buttonClick6 ?? (buttonClick6 = new BaseClass.RelayCommand((p) =>
+                {
+                    game.buttonClick(6);
+                    NextMove = game.nextPlayerString();
+                    string[] temp = (string[])Board.Clone();
+                    temp[6] = game.board[6];
+                    Board = temp;
+                    bool[] temp2 = (bool[])ButtonsUse.Clone();
+                    temp2[6] = game.emptyField[6];
+                    ButtonsUse = temp2;
+                    if (game.checkGame())
+                    {
+                        ButtonsUse = new bool[9] { true, true, true, true, true, true, true, true, true };
+                        Board = new string[9] { "", "", "", "", "", "", "", "", "" };
+                        NextMove = "X\nmove";
+                    }
+                }, p => true));
+            }
+        }
+        public ICommand ButtonClick7
+        {
+            get
+            {
+                return buttonClick7 ?? (buttonClick7 = new BaseClass.RelayCommand((p) =>
+                {
+                    game.buttonClick(7);
+                    NextMove = game.nextPlayerString();
+                    string[] temp = (string[])Board.Clone();
+                    temp[7] = game.board[7];
+                    Board = temp;
+                    bool[] temp2 = (bool[])ButtonsUse.Clone();
+                    temp2[7] = game.emptyField[7];
+                    ButtonsUse = temp2;
+                    if (game.checkGame())
+                    {
+                        ButtonsUse = new bool[9] { true, true, true, true, true, true, true, true, true };
+                        Board = new string[9] { "", "", "", "", "", "", "", "", "" };
+                        NextMove = "X\nmove";
+                    }
+                }, p => true));
+            }
+        }
+        public ICommand ButtonClick8
+        {
+            get
+            {
+                return buttonClick8 ?? (buttonClick8 = new BaseClass.RelayCommand((p) =>
+                {
+                    game.buttonClick(8);
+                    NextMove = game.nextPlayerString();
+                    string[] temp = (string[])Board.Clone();
+                    temp[8] = game.board[8];
+                    Board = temp;
+                    bool[] temp2 = (bool[])ButtonsUse.Clone();
+                    temp2[8] = game.emptyField[8];
+                    ButtonsUse = temp2;
+                    if (game.checkGame())
+                    {
+                        ButtonsUse = new bool[9] { true, true, true, true, true, true, true, true, true };
+                        Board = new string[9] { "", "", "", "", "", "", "", "", "" };
+                        NextMove = "X\nmove";
+                    }
+                }, p => true));
+            }
+        }
+
+    }
+}
